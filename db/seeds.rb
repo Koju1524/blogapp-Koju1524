7af11a99c2da66953e952e0884f89895eeb81adf
Koju Sakaguchi <fuetf@icloud.com>
2020-08-06 10:01:03 +0900

    記事一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create({title: '新しい記事です', content: 'これは素晴らしいです。'})
+Article.create({title: '素晴らしくです', content: 'aaaaaaaaaaaaaaa'})
