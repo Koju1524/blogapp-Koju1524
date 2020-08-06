de32db4158deda95ae0390bfc7ec091decb28098
Koju Sakaguchi <fuetf@icloud.com>
2020-08-06 19:45:27 +0900

    ダミーdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
+#10.times do
 
     Article.create(title: '記事のタイトル', content: '記事の内容です')
 
