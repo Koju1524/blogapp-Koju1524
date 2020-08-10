033c3b5c8466e1cb250e6faac912b2b4ffdc0474
Koju Sakaguchi <fuetf@icloud.com>
2020-08-10 10:42:22 +0900

    記事を更新できるように変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article .save
-         redirect_to article_path(@article), notice: '保存できたよ'
+          redirect_to article_path(@article), notice: '保存できたよ'
         else
          flash.now[:error] = '保存に失敗しました'
          render :new
@@ -23,6 +23,16 @@ class ArticlesController < ApplicationController
 
     def edit
        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+           redirect_to article_path(@article), notice: '更新できました' 
+        else
+         flash.now[:error] = '更新できませんでした'
+         render :edit
+        end
     end
 
     private
