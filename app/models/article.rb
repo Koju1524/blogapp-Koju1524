9d020759d7e5ed50b8137afa946ac73d3c43c2e2
Koju Sakaguchi <fuetf@icloud.com>
2020-08-22 11:16:41 +0900

    いいねの数を表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -29,17 +29,21 @@ class Article < ApplicationRecord
     belongs_to :user
 
     def display_created_at
-        I18n.l(self.created_at, format: :default)
+      I18n.l(self.created_at, format: :default)
     end
 
     def author_name
-        user.display_name
+      user.display_name
+    end
+
+    def like_count
+      likes.count
     end
 
     private
     def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        errors.add(:content, '100文字以上で!') unless char_count > 100
+      char_count = self.title.length + self.content.length
+      errors.add(:content, '100文字以上で!') unless char_count > 100
     end
 
 end
