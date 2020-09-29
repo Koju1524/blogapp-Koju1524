21dd8e8b49aab192b4c2d6089beafe0eda73950e
Koju Sakaguchi <fuetf@icloud.com>
2020-09-29 11:03:12 +0900

    unfollow-function

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,12 +43,7 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user.is_a?(User)
-      user_id = user.id
-    else
-      user_id = user
-    end
-
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
@@ -57,7 +52,8 @@ class User < ApplicationRecord
   end
 
   def unfollow!(user)
-    relation = following_relationships.find.by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -70,6 +66,15 @@ class User < ApplicationRecord
       profile.avatar
     else
      'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
     end
   end
 end
