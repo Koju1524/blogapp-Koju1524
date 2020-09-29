1ce486a8b2bc1aa89b471926a3e802b027a699e8
Koju Sakaguchi <fuetf@icloud.com>
2020-09-29 09:34:40 +0900

    account-page　作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,10 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -40,6 +44,11 @@ class User < ApplicationRecord
 
   def follow!(user)
     following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find.by!(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
