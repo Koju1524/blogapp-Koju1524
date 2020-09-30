5cc9d230135f582c32ed512f74117f86c46888e2
Koju Sakaguchi <fuetf@icloud.com>
2020-09-30 14:07:23 +0900

    Decoratorで書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,10 +38,6 @@ class User < ApplicationRecord
     articles.exists?(id: article.id)
   end
 
-  def display_name
-    profile&.nickname || self.email.split('@').first 
-  end
-
   def follow!(user)
     user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
@@ -59,14 +55,6 @@ class User < ApplicationRecord
 
   def prepare_profile
     profile || build_profile
-  end
-
-  def avatar_image
-    if profile&.avatar&.attached?
-      profile.avatar
-    else
-     'default-avatar.png'
-    end
   end
 
   private
